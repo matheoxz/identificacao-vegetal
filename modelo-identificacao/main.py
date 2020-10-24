8b005ab709ecc5b4772d4e0b2a9c0e5a478b46bd
matheoxz <matheus_sinto@hotmail.com>
2020-10-24 09:35:51 -0300

    início modelo

---
+++ modelo-identificacao/main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import os
+import cv2
+from glob import glob
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
